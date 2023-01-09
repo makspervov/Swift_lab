6303207c8ce4d788767155d0963089319cba4d13
Maksym Pervov <83817268+makspervov@users.noreply.github.com>
2023-01-09 13:04:08 +0100

    Add files via upload

---
+++ lab10/lab10/Osoba.swift
@@ -0,0 +1,24 @@
+//
+//  Osoba.swift
+//  lab10
+//
+//  Created by student on 09/01/2023.
+//
+
+import Foundation
+class Osoba{
+    var Imie: String
+    var Nazwisko: String
+    var rokUrodzenia: Int
+    
+    init(Imie: String, Nazwisko: String, rokUrodzenia: Int) {
+        self.Imie = Imie
+        self.Nazwisko = Nazwisko
+        self.rokUrodzenia = rokUrodzenia
+    }
+    
+    func obliczWiek(rokUrodzenia: rokUrodzenia){
+        var wiek: Int
+        wiek = 2023 - rokUrodzenia
+    }
+}
